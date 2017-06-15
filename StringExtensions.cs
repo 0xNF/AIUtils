066b407e1ec180b5b23d940409c668c6c3f0a8b2
0xnf <nf@Yggdrasil.localdomain>
2017-06-14 20:40:31 -0700

    Pythonic substring

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -19,14 +19,11 @@ namespace AIUtils {
         }
 
         public static string SubstringEndIndex(this string self, int startIndex, int stopIndex) {
-            if(startIndex < self.Length) {
-                throw new IndexOutOfRangeException($"startIndex ({startIndex}) was less than length of the string ({self.Length})");
+            if(startIndex < 0) {
+                throw new IndexOutOfRangeException($"startIndex ({startIndex})was less than zero.");
             }
             if(startIndex >= self.Length) {
                 throw new IndexOutOfRangeException($"startIndex ({startIndex}) was greater than length of the string ({self.Length})");
-            }
-            if (stopIndex < self.Length) {
-                throw new IndexOutOfRangeException($"stopIndex ({stopIndex}) was less than or the length of the string ({self.Length})");
             }
             if (stopIndex >= self.Length) {
                 throw new IndexOutOfRangeException($"stopIndex ({stopIndex}) was greater than or equal to the length of the string ({self.Length})");
@@ -37,7 +34,10 @@ namespace AIUtils {
             if(self.Length == 0 || string.IsNullOrEmpty(self)) {
                 return self;
             }
-            return self.Substring(startIndex, self.Length - stopIndex);
+            if(startIndex == stopIndex) {
+                return string.Empty;
+            }
+            return self.Substring(startIndex, stopIndex-startIndex);
         }
 
 
