a0b8aad96db45fd3db25fe2e5756558fb8f6b8d9
0xnf <nf@Yggdrasil.localdomain>
2017-06-14 17:55:56 -0700

    Added substring stop index

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -18,6 +18,28 @@ namespace AIUtils {
             return m;
         }
 
+        public static string SubstringEndIndex(this string self, int startIndex, int stopIndex) {
+            if(startIndex < self.Length) {
+                throw new IndexOutOfRangeException($"startIndex ({startIndex}) was less than length of the string ({self.Length})");
+            }
+            if(startIndex >= self.Length) {
+                throw new IndexOutOfRangeException($"startIndex ({startIndex}) was greater than length of the string ({self.Length})");
+            }
+            if (stopIndex < self.Length) {
+                throw new IndexOutOfRangeException($"stopIndex ({stopIndex}) was less than or the length of the string ({self.Length})");
+            }
+            if (stopIndex >= self.Length) {
+                throw new IndexOutOfRangeException($"stopIndex ({stopIndex}) was greater than or equal to the length of the string ({self.Length})");
+            }
+            if(stopIndex < startIndex) {
+                throw new IndexOutOfRangeException($"stopIndex ({stopIndex}) was less than or equal to the length of the startIndex ({startIndex})");
+            }
+            if(self.Length == 0 || string.IsNullOrEmpty(self)) {
+                return self;
+            }
+            return self.Substring(startIndex, self.Length - stopIndex);
+        }
+
 
         public static string[] NewLineCharacters = new string[] { "\r\n", "\n" };
 
