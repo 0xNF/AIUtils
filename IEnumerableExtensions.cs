580dce1f5b1294c42c7064417cf7a2065b6e2f0f
0xnf <nf@Yggdrasil.localdomain>
2017-09-13 08:14:39 -0700

    fixed typo

--- IEnumerableExtensions.cs
+++ IEnumerableExtensions.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace AIUtils {
     public static class IEnumerableExtensions {
 
-        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> list) {w
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> list) {
             List<T> flat = new List<T>();
             foreach(IEnumerable<T> sublist in list) {
                 flat.AddRange(sublist);
